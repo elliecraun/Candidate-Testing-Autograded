f2b92ff58ea707af3ee0965bce78ab62a01ef8a8
elliecraun <elliecraun@gmail.com>
2024-06-27 13:31:54 -0500

    Task 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,9 +12,9 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() { 
@@ -24,19 +24,34 @@ candidateName = input.question("Candidate Name: ");
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-console.log(question);
-candidateAnswer = input.question("Your answer: ");
-
+//console.log(question);
+//candidateAnswer = input.question("Your answer: ");
+for (let i = 0; i < questions.length; i++) {
+  //console.log(`${i+1} ${questions[i]}`);
+  candidateAnswers.push(input.question(questions[i]));
+  console.log(`Correct Answer: ${correctAnswers[i]}`);
+    }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer.toLowerCase()===correctAnswer.toLowerCase())
-  console.log("correct answer");
+  //if (candidateAnswer.toLowerCase() === correctAnswer.toLowerCase())
+  //console.log("correct answer");
+  for (let i = 0; i < questions.length; i++) {
+    if((correctAnswers[i]).toLocaleLowerCase() == (candidateAnswers[i]).toLocaleLowerCase()) {
+      console.log(`Question ${i+1} is Correct!`)
+    }
+    else {
+      console.log(`Queston ${i+1} is Incorrect! The correct answer is ${correctAnswers[i]}`)
+      }
+  
+    }
 
 
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+  let grade;  
+ 
+  //TODO 3.2 use this variable to calculate the candidates score.
 
 
   return grade;
@@ -45,7 +60,7 @@ if (candidateAnswer.toLowerCase()===correctAnswer.toLowerCase())
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log("Hello" + candidateName + "!");
+   console.log("Hello " + "" + candidateName + "!");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
